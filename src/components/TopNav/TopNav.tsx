a90298b5a5a2abe37142673df73a58251704dbc6
JuanP-VT <cv.juanp@gmail.com>
2022-08-16 12:06:34 -0600

    Added TopNav component as placeholder

---
+++ src/components/TopNav/TopNav.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+function TopNav({}: Props) {
+  return (
+    <div id="TopNav">
+      <div id="forBusiness">For Business</div>
+      <div id="forIndividuals">For Individuals</div>
+    </div>
+  );
+}
+
+export default TopNav;
