155925c7683133e052a49723df015dd4a7dbbfa3
JuanP-VT <cv.juanp@gmail.com>
2022-08-16 13:12:25 -0600

    Current route is now save properly in App state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import ForBusiness from "./components/ForBusiness/ForBusiness";
 import ForIndividuals from "./components/ForIndividuals/ForIndividuals";
 import TopNav from "./components/TopNav/TopNav";
-
+import { useLocation } from "react-router-dom";
 function App() {
+  // This saves the current route in state
+  const location = useLocation();
+  const [CurrentRoute, setCurrentRoute] = useState(location.pathname);
   return (
     <div className="App">
-      <TopNav />
+      <TopNav setCurrentRoute={setCurrentRoute} />
       <Routes>
         <Route path="/business" element={<ForBusiness />} />
         <Route path="/individuals" element={<ForIndividuals />} />
--- src/components/TopNav/TopNav.tsx
+++ src/components/TopNav/TopNav.tsx
@@ -1,13 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function TopNav() {
+interface Props {
+  setCurrentRoute: React.Dispatch<React.SetStateAction<string>>;
+}
+function TopNav({ setCurrentRoute }: Props) {
+  // This function is will save in the current route in state
+  // whenever one of the two anchor links are clicked
+  function handleClick(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    const target = e.target as HTMLAnchorElement;
+    const route = target.getAttribute("data-route") as string;
+    console.log(route);
+    setCurrentRoute(route);
+  }
   return (
     <div id="TopNav">
-      <Link id="forBusiness" to="/business">
+      <Link
+        id="forBusiness"
+        to="/business"
+        onClick={(e) => handleClick(e)}
+        data-route="/business"
+      >
         For Business
       </Link>
-      <Link id="forIndividuals" to="/individuals">
+      <Link
+        id="forIndividuals"
+        to="/individuals"
+        onClick={(e) => handleClick(e)}
+        data-route="/individuals"
+      >
         For Individuals
       </Link>
     </div>
