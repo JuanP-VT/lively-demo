77bfe1bd6011d4f76abc44d8107e715abcd6597f
JuanP-VT <cv.juanp@gmail.com>
2022-08-16 15:28:24 -0600

    Added content to the business component

--- src/components/ForBusiness/ForBusiness.tsx
+++ src/components/ForBusiness/ForBusiness.tsx
@@ -18,7 +18,20 @@ function ForBusiness() {
           <div>Resources</div>
           <div>About us</div>
           <div>HSA Guide</div>
-          <div className="sales">Contact Sales</div>
+          <button className="greenBtn">Contact Sales</button>
+        </div>
+      </div>
+      <div className="content">
+        <div className="left">
+          <p className="header">HSA administration for employers, simplified</p>
+          <p className="p">
+            Lively takes care of the heavy lifting so you can manage your health
+            saving account program with ease
+          </p>
+          <button className="greenBtn"> Learn More</button>
+        </div>
+        <div className="right">
+          <div className="portrait"></div>
         </div>
       </div>
     </div>
