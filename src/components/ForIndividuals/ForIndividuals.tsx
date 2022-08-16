e1b3797f31e342f45a594f5faa3ec4ccef6516bc
JuanP-VT <cv.juanp@gmail.com>
2022-08-16 16:01:40 -0600

    added header for Individuals component

--- src/components/ForIndividuals/ForIndividuals.tsx
+++ src/components/ForIndividuals/ForIndividuals.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 function ForIndividuals() {
-  return <div id="Individuals">ForIndividuals</div>;
+  return (
+    <div id="Individuals">
+      <div id="individualsHeader">
+        <div className="left">
+          <img
+            src="https://images.ctfassets.net/t1oaprbqkyo4/3vIrNOV5VCmhXApqkXHecf/bf3ed7a5b39312ae7f214a90f6026045/logo.svg"
+            alt="lively logo"
+          />
+          <div>HSA Features</div>
+          <div>Transfer Hsa</div>
+          <div>Invest HSA</div>
+          <div>Pricing</div>
+        </div>
+        <div className="right">
+          <div>Blog</div>
+          <div>Resources</div>
+          <div>About us</div>
+          <div>Log In</div>
+          <button className="greenBtn">Open Account</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ForIndividuals;
