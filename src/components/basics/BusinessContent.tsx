d3c8919cef15957a384375379ad912646df7a813
JuanP-VT <cv.juanp@gmail.com>
2022-08-18 12:17:53 -0600

    Added image modulation

--- src/components/basics/BusinessContent.tsx
+++ src/components/basics/BusinessContent.tsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import imgOne from "../../img/business.avif";
+import imgTwo from "../../img/businessOne.avif";
+import imgThree from "../../img/businessTwo.avif";
 
 function BusinessContent() {
+  const [CurrentIndex, setCurrentIndex] = useState(1);
+  useEffect(() => {
+    // this will loop every 5 seconds
+    const interval = setInterval(() => {
+      console.log(CurrentIndex);
+      const container = document.querySelector(
+        "#portraitInd"
+      ) as HTMLDivElement;
+      const childNodes = container.childNodes;
+      // remove all active class
+      childNodes.forEach((elem) => {
+        const target = elem as HTMLImageElement;
+        target.classList.remove("active");
+      });
+      // add active to current index
+      const target = childNodes[CurrentIndex] as HTMLImageElement;
+      target.classList.add("active");
+      if (CurrentIndex >= 2) {
+        setCurrentIndex(0);
+      } else {
+        setCurrentIndex((CurrentIndex) => CurrentIndex + 1);
+      }
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [CurrentIndex]);
   return (
     <div className="content">
       <div className="left">
@@ -12,7 +40,28 @@ function BusinessContent() {
         <button className="greenBtn"> Learn More</button>
       </div>
       <div className="right">
-        <div className="portrait"></div>
+        <div className="portrait">
+          <div className="portrait" id="portraitInd">
+            <img
+              src={imgOne}
+              alt="individual"
+              className="individualPortrait active"
+              id="indOne"
+            />
+            <img
+              src={imgTwo}
+              alt="individual"
+              className="individualPortrait"
+              id="indTwo"
+            />
+            <img
+              src={imgThree}
+              alt="individual"
+              className="individualPortrait"
+              id="indThree"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/basics/IndividualsContent.tsx
+++ src/components/basics/IndividualsContent.tsx
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import imgOne from "../../img/individual.avif";
+import imgTwo from "../../img/individualOne.avif";
+import imgThree from "../../img/individualTwo.avif";
 
 function IndividualsContent() {
+  // This state will keep track of the current index
+  // in the image slider
+  const [CurrentIndex, setCurrentIndex] = useState(1);
+  useEffect(() => {
+    // this will loop every 5 seconds
+    const interval = setInterval(() => {
+      console.log(CurrentIndex);
+      const container = document.querySelector(
+        "#portraitInd"
+      ) as HTMLDivElement;
+      const childNodes = container.childNodes;
+      // remove all active class
+      childNodes.forEach((elem) => {
+        const target = elem as HTMLImageElement;
+        target.classList.remove("active");
+      });
+      // add active to current index
+      const target = childNodes[CurrentIndex] as HTMLImageElement;
+      target.classList.add("active");
+      if (CurrentIndex >= 2) {
+        setCurrentIndex(0);
+      } else {
+        setCurrentIndex((CurrentIndex) => CurrentIndex + 1);
+      }
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [CurrentIndex]);
   return (
     <div className="content">
       <div className="left">
@@ -12,7 +43,26 @@ function IndividualsContent() {
         <button className="greenBtn">Learn More</button>
       </div>
       <div className="right">
-        <div className="portrait"></div>
+        <div className="portrait" id="portraitInd">
+          <img
+            src={imgOne}
+            alt="individual"
+            className="individualPortrait active"
+            id="indOne"
+          />
+          <img
+            src={imgTwo}
+            alt="individual"
+            className="individualPortrait"
+            id="indTwo"
+          />
+          <img
+            src={imgThree}
+            alt="individual"
+            className="individualPortrait"
+            id="indThree"
+          />
+        </div>
       </div>
     </div>
   );
